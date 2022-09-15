a02b92703069c7ba6bc0c9d3d4faf8a731906eb4
Nicolas Alvarino Laguna <laguna_developer@outlook.com>
2022-09-15 15:50:06 -0500

    Se Sube Repositorio inicial

---
+++ Clase1/src/main/java/org/fundacionview/classone/calculator/console/main/Operation.java
@@ -0,0 +1,21 @@
+package org.fundacionview.classone.calculator.console.main;
+
+public class Operation {
+
+    public int sum(int operatorOne, int operatorTwo){
+        return operatorOne + operatorTwo;
+    }
+
+    public int resta(int operatorOne, int operatorTwo){
+        return operatorOne - operatorTwo;
+    }
+
+    public int multiplicacion(int operatorOne, int operatorTwo){
+        return operatorOne * operatorTwo;
+    }
+
+    public int division(int operatorOne, int operatorTwo){
+        return operatorOne / operatorTwo;
+    }
+
+}
---
+++ apiCalculator/src/test/java/org/fundacionview/springbootclass/calculator/calcilator/ApiCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package org.fundacionview.springbootclass.calculator.calcilator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiCalculatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
